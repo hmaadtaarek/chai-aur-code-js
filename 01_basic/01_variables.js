3f335a26de9ebf1fa215b1a407954f9ebfe379b8
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-01-29 15:38:04 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 134536
+let accountEmail = "helo@gmail.com"
+var accountPassword = "123456789"
+
+accountCity = "lahore"
+let accountState
+
+//we can use js with or without semi-colon at the end.
+
+/*
+this is the second type of comment.
+
+prefer not to use var because of an issue in block scope and functional scope.
+we can also define a variable withoud using the keyword.
+a varibale without a value can be created i.e undefined.
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all code in the newer javascript version.
+
+/*
+--------Primitive Datatypes---------
+number = 2 power 53.
+bigInt
+string = ""
+null = no value found
+
+type of "null" is object.
+
+unidentified = value not assigned
+boolean = false/true
+symbol => we we need unique
+*/
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
+/*
+
+ */
