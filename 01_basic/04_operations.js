c0df57ff50c7a18a39d5f199fa62161eca135ff3
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-01-30 14:45:44 +0000

    operations

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,33 @@
+/**
+ * console.log(2+2)
+ * console.log(2-2)
+ * console.log(2*2)
+ * console.log(2**3)
+ * console.log(2/2)
+ * console.log(2%2)
+*/
+
+let value = 3
+let negVale = -value
+console.log(negVale)
+
+
+let str1 = "hello"
+let str2 = "hammad"
+let str3 = str1 +str2
+//strings cannot be subtrated
+
+console.log("1"+ 2)
+console.log(1 + "2")
+console.log("1"+"2")
+console.log("1"+2 +2)
+console.log(1+2+"2")
+
+// in the last two cases preference to the operation is given to the datatypes of first elements.
+
+console.log(true)
+console.log(+true)
+console.log(false)
+console.log(+false)
+console.log(+"")
+console.log(+"hammad")
