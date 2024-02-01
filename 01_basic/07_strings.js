b1380de359aaa8cb6f71ad6eeae08fe322bfde5e
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-01 16:55:09 +0000

    strings1

---
+++ 01_basic/07_strings.js
@@ -0,0 +1,12 @@
+const name = "hammad"
+const repoCount = 50
+
+console.log(name + repoCount + "value") // old syntax for contatinating strings.
+
+// in modern day we use "string interpolation".
+
+console.log(`Hello my name is ${name}. My repo count is ${repoCount}`)
+
+//how to declare astring?
+const myName = new String ("hammad")
+console.log(myName.)
