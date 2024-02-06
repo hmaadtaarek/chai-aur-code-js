eb454130021900875fe6b0db0dce1eba4f3364fd
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-06 13:46:05 +0000

    math

---
+++ 01_basic/09_math.js
@@ -0,0 +1,7 @@
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.abs(-4)) // return anbsolute value
+console.log(Math.abs(4))
+console.log(Math.round(4.67))
+console.log(Math.floor(4.67)) // round to lower value
+console.log(Math.ceil(4.67)) // ceiling = round to highest value
