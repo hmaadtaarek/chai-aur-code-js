9c4099cf79c8d97dcbc3b9c781b90efe6cfa8510
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-06 14:19:44 +0000

    math

--- 01_basic/09_math.js
+++ 01_basic/09_math.js
@@ -1,16 +1,17 @@
-// console.log(Math)
-// console.log(Math.PI)
-// console.log(Math.abs(-4)) // return anbsolute value
-// console.log(Math.abs(4))
-// console.log(Math.min(4, 5, 10, 47))
-// console.log(Math.max(4, 5, 10, 47))
-// console.log(Math.round(4.67))
-// console.log(Math.floor(4.67)) // round to lower value
-// console.log(Math.ceil(4.67)) // ceiling = round to highest value
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.abs(-4)) // return anbsolute value
+console.log(Math.abs(4))
+console.log(Math.min(4, 5, 10, 47))
+console.log(Math.max(4, 5, 10, 47))
+console.log(Math.round(4.67))
+console.log(Math.floor(4.67)) // round to lower value
+console.log(Math.ceil(4.67)) // ceiling = round to highest value
+console.log(Math.trunc(234.76)) // drop the fractional part of the integer.
 
-// console.log(Math.random()) // retirn random value b/w 0 and 1.
-// console.log((Math.random()*10) + 1)
-// console.log(Math.floor(Math.random()*10) + 1)
+console.log(Math.random()) // retirn random value b/w 0 and 1.
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
 
 const min = 10
 const max = 20
