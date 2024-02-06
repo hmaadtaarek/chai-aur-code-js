a45f864a42f34d21f54bf02b8fd0323d02465d22
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-06 11:40:07 +0000

    strings

--- 01_basic/07_strings.js
+++ 01_basic/07_strings.js
@@ -9,7 +9,18 @@ console.log(`Hello my name is ${name}. My repo count is ${repoCount}`)
 
 //how to declare astring?
 const myName = new String ("hammad")
+
 console.log(myName[0])
 console.log(myName.__proto__)
+
 console.log(myName.at(5))
 console.log(myName.at(-4))
+
+console.log(myName.charAt(5))
+console.log(myName.charAt(-4))
+//charAt gives the empty string when index is given in negative.
+
+console.log(myName.charCodeAt(3))
+//The charCodeAt() method of String values returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
+console.log(myName.charCodeAt(-4)) // NaN
+
