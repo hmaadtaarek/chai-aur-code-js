fa6ff28142f40119ee73cbc4f38ae7f71587795c
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-10 14:39:55 +0000

    date

---
+++ 01_basic/10_date.js
@@ -0,0 +1,14 @@
+//----------date-----
+
+
+let myDate = new Date()
+
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof(myDate)) // object
+
+const myCreatedDate = new Date (2024, 0 ,20) // yyyy,mm,dd
+
+console.log(myCreatedDate.toDateString())
