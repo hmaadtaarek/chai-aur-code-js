f31c52f7cfc08c6fbd5315c20d32272132b2d926
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-12 16:41:28 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,12 @@
+/////----------------------------- Array part 1 ------------------------------------------
+
+
+
+const arr = [1,2,3,4,5]
+
+console.log(arr)
+console.log(arr[2])
+
+const newarr = new Array(1,2,3,4,5,6,7,8,90)
+
+console.log(newarr)
