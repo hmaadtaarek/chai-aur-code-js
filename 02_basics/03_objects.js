094aced3884490e8c7d264f613a65744629d16b4
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-02-14 16:49:38 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,27 @@
+// objects are diclared by 2 methods.
+// 1. By constructer method (singleton is formed.)
+// Object.create
+
+
+//2. by object literals.
+const mysyb = Symbol("key1")
+const JSuser = {
+    name : "Hammad",
+    age : 19,
+    [mysyb] : "mykey",
+    mysyb : "mykey1",
+    email: "hammad@gmail.com",
+    location: "lahore",
+    isLogggedIn: false,
+    loginDays: ['monday','tuesday','saturday']
+}
+
+console.log(JSuser.email)
+console.log(JSuser["email"])
+console.log(JSuser["loginDays"])
+console.log(JSuser[mysyb])
+console.log(typeof JSuser[mysyb])
+console.log(JSuser.mysyb)
+console.log(typeof JSuser.mysyb)
+
+JSuser.email = " hammadtariq@mail.comm"
