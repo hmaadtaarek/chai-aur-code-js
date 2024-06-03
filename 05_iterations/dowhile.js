24ca7f23490ba14d113a42eb6d1f4550f95cc28f
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-06-03 15:18:47 +0000

    dowhile

---
+++ 05_iterations/dowhile.js
@@ -0,0 +1,52 @@
+// it check the condition at te end.
+let score = 1
+
+// do {
+//     console.log(`value is ${score}`)
+//     score++
+// } while (score <=10);
+
+// let value = 11
+// do {
+//     console.log(`value is ${value}`)
+//     value ++
+// } while (value <=10);
+
+
+for (let index = 11; index <=10; index++) {
+    const element = index;
+    console.log(element)
+    
+}
+
+let value = 8
+while (value <=10) {
+    console.log(`value is ${value}`)
+    value ++
+    
+}
+
+
+
+
+
+
+
+// do {
+//     console.log(`value is ${score}`)
+//     score++
+//     if (score ==5) {
+//         console.log("5 detected")
+//         break
+//     }
+// } while (score <=10);
+
+// do {
+//     console.log(`value is ${score}`)
+//     score++
+//     if (score ==5) {
+//         console.log("5 detected")
+//         continue
+//     }
+// } while (score <=10);
+
