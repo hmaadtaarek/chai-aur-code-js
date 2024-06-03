227ee1889651df0c343d522dd2c2bd2a5cc2b756
Hammad Tarek <111991481+hmaadtaarek@users.noreply.github.com>
2024-06-03 15:06:40 +0000

    while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,40 @@
+
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index: ${index}`)
+//     index = index + 1
+// }
+
+// const myArray = ['flash', 'superman', 'hammad']
+
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`)
+//     arr = arr + 1
+    
+// }
+
+// let index = 0
+// while (index <=10) {
+   
+    
+//     if (index == 5) {
+//         console.log(" 5 printed")
+//         break  
+//     }
+   
+//     console.log(`Value of index: ${index}`)
+//     index = index + 1
+// }
+
+let index = 0
+while (index <=10) {
+    index = index + 1
+
+    if (index == 5) {
+        console.log(" 5 printed")
+        continue
+    }   
+    console.log(`Value of index: ${index}`)
+}
